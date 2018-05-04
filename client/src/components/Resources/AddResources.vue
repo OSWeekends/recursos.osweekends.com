949b4e7a25f87b9dbf0067b14e0f3c73da5094d8
trigoporres <santiagotrigoporres@gmail.com>
2018-05-04 18:49:12 +0200

    azance en firebase tipo y empezando categorias

--- client/src/components/Resources/AddResources.vue
+++ client/src/components/Resources/AddResources.vue
@@ -10,7 +10,14 @@
       <label>Url</label>
       <input type='text' v-model='resource.url' placeholder='Url'>
 
+      <select v-model="resource.type">
+        <option disabled value="">Please select one</option>
+        <option v-for="tipo in types" :key="tipo.id">{{tipo}}</option>
+      </select>
+      <span>Selected: {{ resource.type }}</span>
+
       <input type='submit' value='guardar'>
+      <pre>{{ resource }}</pre>
 
     </form>
   </div>
@@ -24,15 +31,21 @@ export default {
   name: 'resources',
   data () {
     return {
+      types: [],
       resource: {
         title: '',
         description: '',
-        url: ''
+        url: '',
+        type: ''
       }
     }
   },
+  created () {
+    firebase.database().ref('type')
+      .once('value', snapshot => { this.types = snapshot.val() })
+  },
   methods: {
-    addResource (tipo) {
+    addResource () {
       axios.get('https://api.microlink.io/?url=https%3A%2F%2F' + this.resource.url + '&screenshot&filter=screenshot')
         .then((response) => {
           firebase.database().ref('Recursos')
@@ -40,12 +53,14 @@ export default {
               title: this.resource.title,
               description: this.resource.description,
               url: this.resource.url,
-              img: response.data.data.screenshot.url
+              img: response.data.data.screenshot.url,
+              type: this.resource.type
             })
             .then(() => {
               this.resource.title = ''
               this.resource.description = ''
               this.resource.url = ''
+              this.resource.type = ''
             })
         })
     }
