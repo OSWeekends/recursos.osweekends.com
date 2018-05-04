62fc288d1013955204e7850daf7e224843757264
Jorgeural <jorgeural@hotmail.com>
2018-05-05 01:30:03 +0200

    Home

--- client/src/main.js
+++ client/src/main.js
@@ -4,9 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import firebase from 'firebase'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import BootstrapVue from 'bootstrap-vue'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 var config = {
   apiKey: 'AIzaSyCV1oei0T8oUoYEy3Uv__wQwLvev5pJ_Jc',
@@ -19,7 +19,7 @@ var config = {
 firebase.initializeApp(config)
 
 Vue.config.productionTip = false
-Vue.use(BootstrapVue)
+// Vue.use(BootstrapVue)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
