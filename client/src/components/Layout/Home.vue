373ef987fecb3c6a89ffa822cbd5564884e3695c
trigoporres <santiagotrigoporres@gmail.com>
2018-07-01 12:48:37 +0200

    medio maquetado about, falta decidir textos y contenido

--- client/src/components/Layout/Home.vue
+++ client/src/components/Layout/Home.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="container">
     <section class="textPresent">
-      <h2>Proyecto</h2>
-      <p>Open Source Weekends somos un grupo de personas que nos gustan hablar de hacer cosas y hacerlas. Por ello en nuestro canal de Slack, al cabo del día, se comparten muchos recursos muy interesantes. Pero Slack los borra y no todo el puedo puede acceder en cualquier momento a ellos. Por eso nace este proyecto, queremos que esos recursos queden guardados y que cualquier persona que desee pueda acceder a ellos. Ademas conoceras quien lo ha compartido, una de las partes que consideramos fundamentales es compartir y aprender unos de otros</p>
+      <h1>Proyecto Recursos</h1>
+      <p>Open Source Weekends somos un grupo de personas que nos gustan hablar de hacer cosas y hacerlas. Por ello en nuestro canal de Slack, al cabo del día, se comparten muchos recursos muy interesantes. Pero Slack los borra y no todo el puedo puede acceder en cualquier momento a ellos. Por eso nace este proyecto, queremos que esos recursos queden guardados y que cualquier persona que desee pueda acceder a ellos. Ademas conoceras quien lo ha compartido, una de las partes que consideramos fundamentales es compartir y aprender unos de otros.</p>
     </section>
     <section class="textExplication">
-      <h3>Comparte</h3>
-      <p>Comparte con la comunidad aquellos recursos que te hayan sido de utilidad en el pasado.</p>
-      <h3>Vota</h3>
-      <p>Si un recurso te ha sido de ayuda, votalo! para que los mejores recursos tengan mayor visibilidad.</p>
-      <h3>Comenta</h3>
-      <p>Comparte tus impresiones y experiencias respecto al recurso, o tema tratado.</p>
-    </section>
-    <section class="icons">
-      <p>eeeeeeee</p>
+      <div class="share">
+        <i class="fas fa-share"></i>
+        <h3>Comparte</h3>
+        <p>Comparte con la comunidad aquellos recursos que te hayan sido de utilidad en el pasado.</p>
+      </div>
+      <div class="thumbs">
+        <i class="far fa-thumbs-up"></i>
+        <h3>Vota</h3>
+        <p>Si un recurso te ha sido de ayuda, votalo! para que los mejores recursos tengan mayor visibilidad.</p>
+      </div>
+      <div class="comments">
+        <i class="fas fa-comments"></i>
+        <h3>Comenta</h3>
+        <p>Comparte tus impresiones y experiencias respecto al recurso, o tema tratado.</p>
+      </div>
     </section>
     <footer>
       Open Source Weekends es una comunidad de gente apasionada por la tecnología.
@@ -36,8 +42,40 @@ export default {
 
 <style scoped>
 .container{
+  height: 100vh;
   display: grid;
   grid-template-columns: 1fr;
-  grid-template-rows: 1fr 1fr 1fr 1fr;
+  grid-template-rows: auto auto auto;
+  grid-gap: 10px;
+  justify-items: center;
+}
+.textPresent{
+  width: 80%;
+}
+h1{
+  text-align: center;
+  margin-bottom: 10px;
+  font-size: 32px;
+}
+.textPresent{
+  line-height: 26px;
+}
+.textExplication{
+  display: flex;
+  flex-direction: columns;
+  width: 80%;
+  text-align: center;
+}
+.fa-share, .fa-thumbs-up, .fa-comments{
+  font-size: 100px;
+  color: #0059f2;
+}
+.textExplication p{
+  width: 80%;
+  margin: 0 auto;
+}
+footer{
+  width: 80%;
+  text-align: center;
 }
 </style>
